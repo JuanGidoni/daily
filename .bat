b8fd2c139fe051626d6ad72ad855cf7d5640291f
juani.dev <juan.gidoni@gmail.com>
2024-12-13 18:33:25 +0100

    create .bat

---
+++ .bat
@@ -0,0 +1,9 @@
+@echo off
+:: Cambia a la ruta del directorio de tu script
+cd C:\ruta\a\tu\proyecto
+
+:: Ejecuta Node.js con el script
+node daily-commit.js
+
+:: Agrega pausa para debug (opcional)
+:: pause
